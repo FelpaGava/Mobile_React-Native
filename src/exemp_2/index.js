579a5750a9d309a537cccc5dd2f42ab6bd1025a4
Marcos Felipe Gava <161661597+FelpaGava@users.noreply.github.com>
2024-02-28 22:43:02 -0300

    Aula 28-02
    Hoje configuramos o Git, GitHub e Visual Studio, onde vamos desenvolver frequentemente.

---
+++ src/exemp_2/index.js
@@ -0,0 +1,9 @@
+import { View,Text } from "react-native";
+
+export default function mensagem () {
+    return(
+        <View>
+            <Text> HELLO REACT-NATIVE</Text>
+        </View>
+    );
+}
