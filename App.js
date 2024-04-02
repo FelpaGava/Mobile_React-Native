d3573185a7f0e023d2cba017de5a90ef08259dcd
Marcos Felipe Gava <161661597+FelpaGava@users.noreply.github.com>
2024-04-02 19:32:00 -0300

    atividade 4

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { StyleSheet, View,} from 'react-native';
 // import Mensagem from './src/exemp_2';
 // import Atividade1 from './src/atividade_1';
 // import Exemplo_3 from './src/exemp_3';
-import Atividade_3 from './src/atividade_3';
+// import Atividade_3 from './src/atividade_3'
+import atividade_4 from './src/atividade_4';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade_3 />
+      <atividade_4 />
       <StatusBar style="auto" />
     </View>
   );
