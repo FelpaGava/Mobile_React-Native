c7b20d0a56f066a9b07e1060fdc5257fbbb81a18
Marcos Felipe Gava <161661597+FelpaGava@users.noreply.github.com>
2024-06-03 21:26:55 -0300

    Update

--- src/exemp_6/index.js
+++ src/exemp_6/index.js
@@ -13,12 +13,25 @@ export default function Exemplo_6() {
     const [ altura, setAltura] = useState(null)
     const [ imc, setImc] = useState(0.00)
     const mensErro = 'Valores inseridos de forma incorreta!'
+    const [msgResult, setResult] = useState();
 
     function calculaImc () {
         
      const tmpImc = peso / (altura*altura)
-     setImc(tmpImc);
-        
+     setImc(tmpImc)
+     if (tmpImc < 18.5){
+        setResult('Abaixo do peso')
+     } else if (tmpImc > 18.5 && tmpImc < 24.90){
+        setResult('Peso normal')
+     } else if (tmpImc > 25 && tmpImc < 29.90){
+        setResult('Sobrepeso')
+     } else if (tmpImc > 30 && tmpImc < 34.90){
+        setResult('Obesidade grau 1')
+     } else if (tmpImc > 35 && tmpImc < 39.90){
+        setResult('Obesidade grau 2')
+     } else if (tmpImc > 40){
+        setResult('Obesidade grau 3')
+     } 
     }
 
     return(
@@ -30,6 +43,8 @@ export default function Exemplo_6() {
         </View>
 
         <Text style={styles.imc}>{isNaN(imc) ? mensErro : imc.toFixed(2)}</Text>
+
+        <Text style={styles.imc}>{msgResult}</Text>
 
         <Botao calcular={calculaImc}>Calcular</Botao>
     </View>
